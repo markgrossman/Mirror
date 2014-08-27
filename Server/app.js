5ee7ff5e110830f2956ab6b4a48e64702680ae72
Mark Grossman <mark.grossman@bpiedu.com>
2014-08-27 14:46:32 -0700

    Rename index.js to app.js and add ping route

--- Server/app.js
+++ Server/app.js
@@ -2,6 +2,11 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.get('/ping', function(req, res){
+  res.send('<h1>Pong!</h1>');
+});
+
+
 io.on('connection', function(socket) {
     socket.on('textBox', function(data) {
         socket.broadcast.emit('textBox', data);
